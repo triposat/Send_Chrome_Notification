1b88b3455c10e3819b52b9e9c609f862ab773248
Satyam Tripathi <satyamtripathi5554@gmail.com>
2021-08-01 22:14:30 +0530

    Create notification.py

---
+++ notification.py
@@ -0,0 +1,11 @@
+# First run it for getting registered!
+from notify_run import Notify
+  
+notify = Notify()
+notify.register()
+
+# Then comment the above code, and run the below code!
+from notify_run import Notify
+  
+notify = Notify()
+notify.send("Any Message you want to send")
